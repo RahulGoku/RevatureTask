8ea33617f39ae6677a44a8f6ca693f2589ec2fa3
RahulGoku <rrjn8987@gmail.com>
2021-06-29 22:09:15 +0530

    Day1 Task

---
+++ Operator_Loop_Day2/src/operator/Loop_Operator.java
@@ -0,0 +1,43 @@
+package operator;
+import java.util.*;
+
+public class Loop_Operator {
+public static void main(String[] args) {
+	int a,b,choice;
+	Operator_test o = new Operator_test();
+	System.out.println("Press 1 for Comparing the Number ");
+	System.out.println("Press 2 for Logical Operator ");
+	System.out.println("Press 3 Find the power  ");
+	System.out.println("Press 4 Shift Operator");
+	System.out.println("Press 5 for The Table of a  Number ");
+	System.out.println("Press 6 for Time of Journey ");
+	try (Scanner sc = new Scanner(System.in)) {
+		System.out.println("Enter the Value of a and b and choice: ");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		choice=sc.nextInt();
+	}
+	
+    switch(choice) {
+    case 1: o.compareNumber(a, b);
+            break;
+    case 2: o.logicalOperator(a, b);
+            break;
+    case 3: o.powerOfNumber(a, b);
+            break;
+    case 4: o.shiftNumber(a, b);
+            break;
+    case 5: o.Table(a);
+     		break;
+    case 6: o.journeyTime(a,b);
+     		break;
+    default : System.out.println("Enter the valid Key :");
+              break;
+    
+    
+    }
+	
+     
+	
+}
+}
